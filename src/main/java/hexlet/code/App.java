32d93d1cd751266f64c9fba37dd35cdc7f9d1543
andreevmo <maxiandreev1994@gmail.com>
2022-08-09 21:37:03 +0400

    feat: update App
    Add output command line help in class App and preparation Differ

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,13 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
 public class App {
     public static void main(String[] args) {
+        System.exit(new CommandLine(new App()).execute(args));
         System.out.println("Hello world!");
     }
 }
