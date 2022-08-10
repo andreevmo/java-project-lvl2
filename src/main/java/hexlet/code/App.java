b482c1f6bd037d1eb641da14188c6fa315831126
andreevmo <maxiandreev1994@gmail.com>
2022-08-10 21:06:23 +0400

    feat: add Option and Parameters
    Add option "format" and parameters "filepath1","filepath2"

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,10 +2,20 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App {
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
+    private String format;
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private File filepath2;
     public static void main(String[] args) {
         System.exit(new CommandLine(new App()).execute(args));
         System.out.println("Hello world!");
