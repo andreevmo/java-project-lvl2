0afe139c423763e37a2e7b01a2d20b04908aa646
andreevmo <maxiandreev1994@gmail.com>
2022-08-11 13:11:31 +0400

    feat: update App, Differ
    Add a function to compare flat json files

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,19 +5,26 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.io.File;
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App {
+public class App implements Callable {
     @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
     private String format;
     @Parameters(index = "0", description = "path to first file")
-    private File filepath1;
+    private Path filepath1;
     @Parameters(index = "1", description = "path to second file")
-    private File filepath2;
+    private Path filepath2;
     public static void main(String[] args) {
         System.exit(new CommandLine(new App()).execute(args));
         System.out.println("Hello world!");
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println(Differ.generate(filepath1, filepath2));
+        return 0;
     }
 }
--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,8 +1,65 @@
 package hexlet.code;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Differ {
 
-    public static String generate() {
-        return "";
+    public static String generate(Path filepath1, Path filepath2) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> parseFileOne = mapper.readValue(filepath1.toAbsolutePath().toFile(), Map.class);
+        Map<String, Object> parseFileSecond = mapper.readValue(filepath2.toAbsolutePath().toFile(), Map.class);
+        return mapToFormatString(compareFiles(parseFileOne, parseFileSecond));
+    }
+
+    private static Map<String, Object> compareFiles(Map<String, Object> parseFileOne,
+                                                    Map<String, Object> parseFileSecond)
+    {
+        Comparator<String> comparator = new Comparator<String>(){
+            @Override
+            public int compare(String s, String t1) {
+                if (s.equals(t1)) {
+                    return 1;
+                }
+                return s.compareTo(t1);
+            }
+        };
+        Map<String, Object> resultMap = new TreeMap<>(comparator);
+        for (Map.Entry element : parseFileOne.entrySet()) {
+            if (parseFileSecond.containsKey(element.getKey())
+                    && parseFileSecond.get(element.getKey()).equals(element.getValue())) {
+                resultMap.put((String) element.getKey(), "  :" + element.getValue());
+            } else if (parseFileSecond.containsKey(element.getKey())
+                    && !(parseFileSecond.get(element.getKey()).equals(element.getValue()))) {
+                resultMap.put((String) element.getKey(), "- :" +element.getValue());
+                resultMap.put((String) element.getKey(), "+ :" + parseFileSecond.get(element.getKey()));
+            } else if (!(parseFileSecond.containsKey(element.getKey()))) {
+                resultMap.put((String) element.getKey(),"- :" + element.getValue());
+            }
+        }
+        for (Map.Entry element : parseFileSecond.entrySet()) {
+            if (!parseFileOne.containsKey(element.getKey())) {
+                resultMap.put((String) element.getKey(), "+ :" + element.getValue());
+            }
+        }
+        return resultMap;
+    }
+
+    private static String mapToFormatString(Map<String, Object> resultMap) {
+        StringBuilder result = new StringBuilder("{");
+        for (Map.Entry<String, Object> element : resultMap.entrySet()) {
+            String[] partsValue = element.getValue().toString().split(":");
+            result.append("\n  ")
+                    .append(partsValue[0])
+                    .append(element.getKey())
+                    .append(": ")
+                    .append(partsValue[1]);
+        }
+        return result.append("\n}").toString();
     }
 }
