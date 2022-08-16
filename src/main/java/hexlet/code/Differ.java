4da1eac0983ce191894cf0d20ec375d0c315e1a6
andreevmo <maxiandreev1994@gmail.com>
2022-08-17 00:17:07 +0400

    refactor: update Differ, Parser, Json

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,49 +1,71 @@
 package hexlet.code;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Differ {
 
     public static String generate(String filepath1, String filepath2, String format) throws IOException {
-        Map<String, Object> fileOne = Parser.parseFile(filepath1);
-        Map<String, Object> fileSecond = Parser.parseFile(filepath2);
+        if (!isCorrectFormat(filepath1, filepath2)) {
+            return "wrong file format";
+        }
+        String text1 = Files.lines(Path.of(filepath1))
+                .collect(Collectors.joining("\n"));
+        String text2 = Files.lines(Path.of(filepath2))
+                .collect(Collectors.joining("\n"));
+        Map<String, Object> fileOne = Parser.parseFile(text1);
+        Map<String, Object> fileSecond = Parser.parseFile(text2);
         Map<String, List<Object>> comparisonResult = compareFiles(fileOne, fileSecond);
         return Formatter.format(comparisonResult, format);
+    }
+
+    public static boolean isCorrectFormat(String filepath1, String filepath2) {
+        return (filepath1.endsWith(".json") && filepath2.endsWith(".json"))
+                || (filepath1.endsWith(".yml") && filepath2.endsWith(".yml"));
     }
 
     public static String generate(String filepath1, String filepath2) throws IOException {
         return generate(filepath1, filepath2, "stylish");
     }
 
-    private static Map<String, List<Object>> compareFiles(Map<String, Object> fileOne, Map<String, Object> fileSecond) {
-        Map<String, List<Object>> result = new TreeMap<>((s1, s2) -> s1.equals(s2) ? 1 : s1.compareTo(s2));
-        for (Map.Entry e : fileOne.entrySet()) {
-            if (fileSecond.containsKey(e.getKey()) && objectsEquals(e.getValue(), fileSecond.get(e.getKey()))) {
-                result.put((String) e.getKey(), createValue("unchanged", e.getValue()));
-            } else if (fileSecond.containsKey(e.getKey()) && !objectsEquals(e.getValue(), fileSecond.get(e.getKey()))) {
-                result.put((String) e.getKey(), createValue("update", e.getValue(), fileSecond.get(e.getKey())));
+    static Map<String, List<Object>> compareFiles(Map<String, Object> fileOne, Map<String, Object> fileSecond) {
+        Map<String, List<Object>> result = new TreeMap<>();
+        Set<String> setKey = new HashSet<>(fileOne.keySet());
+        setKey.addAll(fileSecond.keySet());
+        for (String key : setKey) {
+            if (isUnchanged(fileOne, fileSecond, key)) {
+                result.put(key, createValue("unchanged", fileOne.get(key)));
+            } else if (isUpdate(fileOne, fileSecond, key)) {
+                result.put(key, createValue("update", fileOne.get(key), fileSecond.get(key)));
+            } else if (isRemove(fileOne, fileSecond, key)) {
+                result.put(key, createValue("remove", fileOne.get(key)));
             } else {
-                result.put((String) e.getKey(), createValue("remove", e.getValue()));
-            }
-        }
-        for (Map.Entry e : fileSecond.entrySet()) {
-            if (!fileOne.containsKey(e.getKey())) {
-                result.put((String) e.getKey(), createValue("add", e.getValue()));
+                result.put(key, createValue("add", fileSecond.get(key)));
             }
         }
         return result;
     }
 
-    private static boolean objectsEquals(Object o1, Object o2) {
-        if (o1 != null) {
-            return o1.equals(o2);
-        }
-        return  o2 == null;
+    private static boolean isUnchanged(Map<String, Object> f1, Map<String, Object> f2, String key) {
+        return f1.containsKey(key) && f2.containsKey(key) && Objects.equals(f1.get(key), f2.get(key));
+    }
+
+    private static boolean isUpdate(Map<String, Object> f1, Map<String, Object> f2, String key) {
+        return f1.containsKey(key) && f2.containsKey(key) && !Objects.equals(f1.get(key), f2.get(key));
+    }
+
+    private static boolean isRemove(Map<String, Object> f1, Map<String, Object> f2, String key) {
+        return f1.containsKey(key) && !f2.containsKey(key);
     }
 
     private static List<Object> createValue(Object... value) {
--- src/main/java/hexlet/code/Parser.java
+++ src/main/java/hexlet/code/Parser.java
@@ -3,30 +3,21 @@ package hexlet.code;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
 
-    public static Map<String, Object> parseFile(String address) throws IOException {
-        Map<String, Object> parseFile = new HashMap<>();
-        Path filepath = Path.of(address).toAbsolutePath();
+    public static Map<String, Object> parseFile(String text) throws IOException {
+        Map<String, Object> parseFile;
         ObjectMapper mapper = new ObjectMapper();
-        if (isJson(filepath)) {
-            parseFile = mapper.readValue(filepath.toFile(), Map.class);
-        } else if (isYaml(filepath)) {
+        if (!isJson(text)) {
             mapper = new ObjectMapper(new YAMLFactory());
-            parseFile = mapper.readValue(filepath.toFile(), Map.class);
         }
+        parseFile = mapper.readValue(text, Map.class);
         return parseFile;
     }
 
-    public static boolean isJson(Path filepath) {
-        return filepath.toString().endsWith("json");
-    }
-
-    public static boolean isYaml(Path filepath) {
-        return filepath.toString().endsWith("yml");
+    public static boolean isJson(String text) {
+        return text.charAt(0) == '{';
     }
 }
